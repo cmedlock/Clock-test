1e9f379f74eac9329054f0a0c103ee4bf156c86c
cmedlock <cmedlock@RLE-EECS-MTL-DHCP-20-249.MIT.EDU>
2016-02-03 12:48:10 -0500

    sanity check for all-pole modeling

--- all_pole_modeling_test.py
+++ all_pole_modeling_test.py
@@ -4,6 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.linalg
+import scipy.signal
 import os
 from pylab import *
 
@@ -16,31 +17,37 @@ yneg_uc = -ypos_uc
 
 # target signal
 n = np.arange(100)
-x = 0.5**n
+#x = 0.5**n
+x = np.sin(2*pi/10*n)
+x = np.concatenate((x[3:],x[:3])) # shift so that x[0] is non-zero
 
 # deterministic autocorrelation
 rxx = []
 for w in range(2*len(x)-1):
     val = 0
+    if w<5:
+        print 'w = ',w
     for d in range(len(x)):
         if -1<99-w+d<100:
+            if w<5:
+                print '***  ',d,99-w+d
             val += x[d]*x[99-w+d]
     rxx.append(val)
 
 # model order
-#p = 1 # fraction of E_g in g[0] =  1.0
+#p = 1 # Ediff =  51.5207233409
 #W = rxx[99]
 #D = rxx[100]
-#p = 2 # fraction of E_g in g[0] =  1.0
-#W = np.array([[rxx[99],rxx[100]],[rxx[100],rxx[99]]])
-#D = np.array([[rxx[100]],[rxx[101]]])
-#p = 3 # fraction of E_g in g[0] =  0.977961572123
+p = 2 # Ediff =  57.5531258058
+W = np.array([[rxx[99],rxx[100]],[rxx[100],rxx[99]]])
+D = np.array([[rxx[100]],[rxx[101]]])
+#p = 3 # Ediff = 63.76142098
 #W = np.array([[rxx[99],rxx[100],rxx[101]],[rxx[100],rxx[99],rxx[109]],[rxx[191],rxx[109],rxx[99]]])
 #D = np.array([[rxx[100]],[rxx[101]],[rxx[102]]])
-p = 4 # fraction of E_g in g[0] =  1.0
-W = np.array([[rxx[99],rxx[100],rxx[101],rxx[102]],[rxx[100],rxx[99],rxx[100],rxx[101]],[rxx[101],rxx[100],rxx[99],rxx[100]],
-             [rxx[102],rxx[101],rxx[100],rxx[99]]])
-D = np.array([[rxx[100]],[rxx[101]],[rxx[102]],[rxx[103]]])
+#p = 4 # Ediff =  107.494904876
+#W = np.array([[rxx[99],rxx[100],rxx[101],rxx[102]],[rxx[100],rxx[99],rxx[100],rxx[101]],[rxx[101],rxx[100],rxx[99],rxx[100]],
+#             [rxx[102],rxx[101],rxx[100],rxx[99]]])
+#D = np.array([[rxx[100]],[rxx[101]],[rxx[102]],[rxx[103]]])
 
 if p>1:
     W_inv = np.linalg.inv(W)
@@ -67,8 +74,26 @@ print 'fraction of E_g in g[0] = ',g[0]**2/sum(g**2)
 
 # plot zeros of linear prediction filter
 zeros = 1/np.roots(a[::-1])
+print 'norms of zeros = ',np.abs(zeros)
+print 'angles of zeros = ',np.angle(zeros)
+print '*** compare with 2*pi/10 = ',2*pi/10
 real = [elt.real for elt in zeros]
 imag = [elt.imag for elt in zeros]
+
+# form impulse response of all-pole filter
+# (a.k.a. all-pole model of target signal)
+num = 1.
+den = a # [1.,0.]
+dt = 1.
+t,h = scipy.signal.dimpulse((num,den,dt),n=len(x)+1)
+h = list(h[0])
+h = [float(elt) for elt in h][1:] # for some reason h[0] is always 0
+print 'h[:5] = ',h[:5]
+print 'h.index(max(h)) = ',h.index(max(h))
+# compare model to target signal
+diff = x-h
+Ediff = sum(diff**2)
+print 'Ediff = ',Ediff
 
 plt.close('all')
 
@@ -94,5 +119,17 @@ ax3.axhline(y=0, color='b')
 ax3.set_xlabel('real')
 ax3.set_ylabel('imag')
 plt.axis('equal')
+
+fig3 = plt.figure()
+ax4 = fig3.add_subplot(211)
+ax4.stem(h,label='h[n]')
+ax4.set_xlim(left=-1,right=len(h))
+ax4.set_ylim(top=max(h)*1.2)
+ax4.legend(loc='best',frameon=False)
+ax5 = fig3.add_subplot(212)
+ax5.stem(x,label='x[n]')
+ax5.set_xlim(left=-1,right=len(x))
+ax5.set_ylim(top=max(x)*1.2)
+ax5.legend(loc='best',frameon=False)
 
 plt.show()
