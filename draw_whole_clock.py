af098a9b4f261ac2c7d06bafa935ddf98d6456fa
cmedlock <cmedlock@RLE-EECS-MTL-DHCP-20-249.MIT.EDU>
2016-02-04 16:49:33 -0500

    ordering the symbols chronologically according to when they were started

--- draw_whole_clock.py
+++ draw_whole_clock.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# draw the entire clock, including numbers and clock hands
 import math
 import matplotlib
 import matplotlib.pyplot as plt
@@ -17,7 +18,7 @@ if not os.path.exists(path+'figs_raw'):
     os.makedirs(path+'figs_raw')
 
 # make figs
-for fname in dirs:
+for fname in dirs[:2]:
     if 'Scored' not in fname:
         continue
     print 'reading file ',fname,'...'
@@ -33,7 +34,7 @@ for fname in dirs:
     x_temp,y_temp,t_temp = [],[],[]
 
     # read in data
-    record,found_clock = False,False
+    record,found_clock,is_noise = False,False,False
     for w in range(len(data)):
         line = data[w]
         # found pen stroke?
@@ -48,12 +49,23 @@ for fname in dirs:
         elif record==True:
             if 'RawData' in line and len(x)>0:
                 break
+            if 'symbol label' in line and len(x_temp)==0 and 'NOISE' in line:
+                is_noise = True
             if 'symbol label' in line and len(x_temp)>0: # length requirement since sometimes there are empty strokes
+                #print 'storing previous stroke from ',t_temp[0],' to ',t_temp[-1],' (',(t_temp[0]-1307115563146.0)/10.,' to ',(t_temp[-1]-1307115563146.0)/10.,')'
+                #print 'len(t_temp) is now ',len(t_temp)
+                #print 'next stroke is ',line
                 # store previous stroke and reset lists for the next one
-                x.append(x_temp)
-                y.append(y_temp)
-                t.append(t_temp)
+                if is_noise==False:
+                    x.append(x_temp)
+                    y.append(y_temp)
+                    t.append(t_temp)
+                elif is_noise==True:
+                    is_noise = False
+                print 'len(t) is now ',len(t)
                 x_temp,y_temp,t_temp = [],[],[]
+                if 'NOISE' in line:
+                    is_noise = True
                 continue
             elif 'point' not in line:
                 continue
@@ -71,6 +83,24 @@ for fname in dirs:
         t_temp.append(timestamp)
     
     f.close()
+
+    # force all coordinates to be non-negative
+    xmin = min([min(elt) for elt in x])
+    ymin = min([min(elt) for elt in y])
+    tmin = min([min(elt) for elt in t])
+    x = [np.array(elt)-xmin for elt in x]
+    y = [np.array(elt)-ymin for elt in y]
+    t = [(np.array(elt)-tmin)/10. for elt in t] # the time array should have reasonable values
+    # order the strokes chronologically
+    stroke_start_times = [elt[0] for elt in t]
+    stroke_start_times.sort()
+    stroke_order = []
+    for time in stroke_start_times:
+        for w in range(len(t)):
+            if t[w][0]==time:
+                stroke_order.append(w)
+    for stroke_num in stroke_order:
+        print 'stroke ',stroke_num,': ',t[stroke_num][0],' to ',t[stroke_num][-1]
 
     xmax = max([max(elt) for elt in x])
     # plot
