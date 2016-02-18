bdb23d179895206efacdcf36588c512d382b8a0f
cmedlock <cmedlock@RLE-EECS-MTL-DHCP-28-118.MIT.EDU>
2016-02-18 09:16:49 -0500

    a little bit closer to animating the clock from a different 3d angle

--- draw_whole_clock_angle2d.py
+++ draw_whole_clock_angle2d.py
@@ -6,9 +6,21 @@ import math
 import matplotlib
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as anim
 import numpy as np
 import os
 from pylab import *
+
+def update_lines(num, dataLines, lines):
+    for line, data in zip(lines, dataLines):
+        # NOTE: there is no .set_data() for 3 dim data...
+        line.set_data(data[0:2, :num])
+        line.set_3d_properties(data[2, :num])
+    return lines
+
+# set-up to save animations
+plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
+FFwriter = anim.FFMpegWriter(fps=1000./13.)
 
 # path to data
 path = '/Users/cmedlock/Documents/DSP_UROP/all_data/'
@@ -19,7 +31,7 @@ if not os.path.exists(path+'figs_raw'):
     os.makedirs(path+'figs_raw')
 
 # make figs
-for fname in dirs[:2]:
+for fname in dirs[4:5]:
     if 'Scored' not in fname:
         continue
     print 'reading file ',fname,'...'
@@ -86,47 +98,52 @@ for fname in dirs[:2]:
     
     f.close()
 
-    # force all coordinates to be non-negative
+    # change x so that if the whole clock is drawn,
+    # it is oriented correctly
+    xmax = max([max(elt) for elt in x])
+    for w in range(len(x)):
+        x[w] = [xmax+10-elt for elt in x[w]]
+    xmax = max([max(elt) for elt in x])
     xmin = min([min(elt) for elt in x])
+    ymax = max([max(elt) for elt in y])
     ymin = min([min(elt) for elt in y])
     tmin = min([min(elt) for elt in t])
-    x = [list(np.array(elt)-xmin) for elt in x]
-    y = [list(np.array(elt)-ymin) for elt in y]
-    t = [list((np.array(elt)-tmin)/10.) for elt in t] # the time array should have reasonable values
+    # also normalize the size of the word such that all coordinates
+    # are linearly positioned between 0 and 127
+    for w in range(len(x)):
+        x[w] = [127*(elt-xmin)/(xmax-xmin) for elt in x[w]]
+        y[w] = [127*(elt-ymin)/(ymax-ymin) for elt in y[w]]
+        t[w] = [(elt-tmin)/10. for elt in t[w]] # the time array should have reasonable values
     
     # make each of x, y, and t into a single list for animation purposes
+    # insert NaN's between the symbols so that they are not connected together
     x_long,y_long,t_long = x[0],y[0],t[0]
     for w in range(1,len(x)):
         x_long = x_long+x[w]
         y_long = y_long+y[w]
         t_long = t_long+t[w]
-    
     # rename
-    x,y,t = x_long,y_long,t_long
-    x,y,t = np.array(x),np.array(y),np.array(t)
-    x = max(x)+10-x # ensures that the clock is plotted upright when in 2 dimensions
-    
+    x,y,t = np.array(x_long),np.array(y_long),np.array(t_long)
+
     # order the points by the sum of their x and y coordinates
     sum_xy = []
     for w in range(len(x)):
         sum_xy.append((w,x[w]+y[w]))
     sum_xy = sorted(sum_xy,key=lambda elt: elt[1])
     point_order = [elt[0] for elt in sum_xy]
-    x = [x[idx] for idx in point_order]
-    y = [y[idx] for idx in point_order]
-    t = [t[idx] for idx in point_order]
-        
+    x = np.array([x[idx] for idx in point_order])
+    y = np.array([y[idx] for idx in point_order])
+    t = np.array([t[idx] for idx in point_order])
+
     # plot
     plt.close('all')
     fig_xy = plt.figure()
     fig_xy.text(0.99, 0.96, fname[:len(fname)-4],fontsize=10,color='red',va='baseline',ha='right',multialignment='left')
-    xy = fig_xy.add_subplot(111)
-    xy.scatter(y,x,color='blue')
+    xy = fig_xy.add_subplot(111,projection='3d')
+    #xy.plot(y,x,t,color='blue')
     xy.set_xlabel('x',fontsize=20)
     xy.set_ylabel('y',fontsize=20)
-    xy.set_xlim(min(y)-10,max(y)+10)
-    xy.set_ylim(min(x)-10,max(x)+10)
-    plt.axis('equal')
+    xy.set_zlabel('t',fontsize=20)
     
     if 'YDU' in fname:
         fig_xy.text(0.25, 0.955, 'HEALTHY',fontsize=15,color='black',va='baseline',ha='right',multialignment='left')
@@ -134,8 +151,23 @@ for fname in dirs[:2]:
         fig_xy.text(0.25, 0.955, 'IMPAIRED',fontsize=15,color='black',va='baseline',ha='right',multialignment='left')
     else:
         print 'not a valid filename'
+
+    data = [np.array([y,x,t])] # connect all symbols
+    lines = [xy.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in data]
+
+    xy_anim = anim.FuncAnimation(fig_xy, update_lines, len(x), fargs=(data, lines), interval=13, blit=False) # connect all symbols
+
+    xy.view_init(90,0) # check the regular animation (projection onto the xy-plane)
     
+    # axis limits need to be set after the animation is drawn (not true for a 3d figure)
+    xy.set_xlim(-10,140)
+    xy.set_ylim(-10,140)
+    xy.set_zlim(-10,4500)
+
+    plt.draw() # don't delete this
+    #xy_anim.save(path+'figs_raw/'+fname[:len(fname)-4]+'/'+clock_type+'_whole_clock_anim_angle2d_'+fname[:len(fname)-4]+'.mp4',
+    #             writer=FFwriter,extra_args=['-vcodec','libx264'])
+
     plt.show()
     
     #fig_xy.savefig(path+'figs_raw/'+fname[:len(fname)-4]+'/whole_'+clock_type+'_clock_angle2d_'+fname[:len(fname)-4]+'.png')
-    
